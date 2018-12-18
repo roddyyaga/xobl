f57f711071bc6b5b5a8ac53daa5fa3bf0f8753ef
steinuil <steenuil.owl@gmail.com>
2018-12-18 15:31:44 +0100

    got past the typechecker

--- ocaml_backend/generator.ml
+++ ocaml_backend/generator.ml
@@ -11,27 +11,30 @@ module Size = struct
     | Int32 | Card32 | Float | Xid -> 4
     | Card64 | Double -> 8
 
-  let rec of_x_type ~exts ext_name =
+  let rec of_x_type ~exts ext_name : Types.x_type -> int =
     let open Types in function
-    | Prim t -> of_prim
+    | Prim t -> of_prim t
     | Ref id -> of_ident ~exts ext_name id
 
-  and of_ident ~exts ext_name =
-    let open Types in function
-    | Id n | Ref (ext_name, n) ->
-      let ext = String_map.find ext_name exts in
-      CCList.find_map (of_item ~exts ext_name) ext.declarations
-      |> CCOpt.get_lazy (fun () ->
-        Format.ksprintf invalid_arg "not found in %s: %s"
-          ext_name id
-      )
-  
+  and of_ident ~exts ext_name ident : int =
+    let open Types in
+    let n, ext_name = match ident with
+      | Id n -> (ext_name, n)
+      | Ext_id (ext_name, n) -> (ext_name, n)
+    in
+    let ext = String_map.find ext_name exts in
+    CCList.find_map (of_item ~exts ext_name n) ext.declarations
+    |> CCOpt.get_lazy (fun () ->
+      Format.ksprintf invalid_arg "not found in %s: %s"
+        ext_name n
+    )
+
   and of_pad : Parser.pad -> int = function
     | `Bytes n -> n
     | `Align _ -> 0
 
   and of_padding : Parser.padding -> int =
-    fun Parser.{ pd_pad; pd_serialize } ->
+    fun Parser.{ pd_pad; _ } ->
       of_pad pd_pad
 
   and of_field_type ~exts ext_name : P1_resolve.field_type -> int =
@@ -41,15 +44,18 @@ module Size = struct
     | Enum_or (_, t) | Mask_or (_, t) ->
       of_x_type ~exts ext_name t
 
-  and of_static_fields ~exts ext_name : P2_fields.static_field -> int =
-    function
-    | `Pad p -> of_padding p
-    | `Field (_, typ)
-    | `List_length (_, typ) -> of_field_type ~exts ext_name typ
-    | `List (name, typ, expr) ->
-      Format.ksprintf invalid_arg
-        "not implementing: trying to get size of list %s.%s"
-        ext_name name
+  and of_static_fields ~exts ext_name : P2_fields.static_field list -> int =
+    fun ls ->
+      ls |> List.map (function
+        | `Pad p -> of_padding p
+        | `Field (_, typ)
+        | `List_length (_, typ) -> of_field_type ~exts ext_name typ
+        | `List (name, _typ, _expr) ->
+          Format.ksprintf invalid_arg
+            "not implementing: trying to get size of list %s.%s"
+            ext_name name
+      )
+      |> List.fold_left ( + ) 0
 
   and of_switch_field ~exts ext_name = function
     | Some (_, s) -> of_switch ~exts ext_name s
@@ -58,7 +64,7 @@ module Size = struct
   and of_switch ~exts ext_name : P2_fields.switch -> int =
     fun P2_fields.{ sw_cases; _ } ->
       List.fold_left
-        (fun (acc, x) -> CCInt.max acc (of_case x)) 0
+        (fun acc x -> CCInt.max acc (of_case ~exts ext_name x)) 0
         sw_cases
 
   and of_case ~exts ext_name : P2_fields.case -> int =
@@ -94,7 +100,7 @@ module Size = struct
     | `Struct (name, fields) when name = id ->
       Some (of_struct_fields ~exts ext_name fields)
 
-    | `Union (name, static_fields) when name = id ->
+    | `Union (name, _static_fields) when name = id ->
       invalid_arg ("unsupported: trying to get size of union " ^ id)
 
     | `Event (name, _, ev) when name = id ->
@@ -106,7 +112,7 @@ module Size = struct
     | `Error (name, _, err) when name = id ->
       Some (of_error ~exts ext_name err)
 
-    | `Request (name, _, req) when name = id ->
+    | `Request (name, _, _req) when name = id ->
       invalid_arg ("unsupported: trying to get size of request " ^ id)
       (* Some (of_request ~exts ext_name req) *)
 

f7bd15d695fe2a8a573b4bf2e3c40fd2ff1cd036
steinuil <steenuil.owl@gmail.com>
2020-10-26 12:22:29 +0100

    disabled integration test

--- examples/create_window.ml
+++ examples/create_window.ml
@@ -1,1 +1,5 @@
-let () = Lwt_main.run (Lwt.return ())
+let () =
+  Lwt_main.run
+    (let%lwt resp, _ = Xobl_bindings.X.open_display (Some ":0") in
+     print_endline (Xobl_bindings.Protocol.show_handshake_resp resp);
+     Lwt.return ())
